82d2660cc775a72a5f68a9f7c313f31e2c471140
Natan Nunes <nunesnatan905@gmail.com>
2026-08-18 10:56:13 -0300

    fix: erro de digitação de "download"

--- web-frontend/react-vtstudio/src/components/Navbar.jsx
+++ web-frontend/react-vtstudio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
         <header>
             <nav>
                 <ul>
-                    <li><a href="">Dowload</a></li>
+                    <li><a href="">Download</a></li>
                     <li><a href="">VTStudio</a></li>
                     <li><a href=""><img src="../../src/assets/imgs/vtstudiologo.png" alt=""/></a></li>
                     <li><a href="">GitHub</a></li>
