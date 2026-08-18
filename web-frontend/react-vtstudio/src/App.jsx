13afa331d1a36942c7ffa1b37853d0a686e80b26
Natan Nunes <nunesnatan905@gmail.com>
2026-08-18 10:58:36 -0300

    feat: adiciona react-router-dom para navegação

--- web-frontend/react-vtstudio/src/App.jsx
+++ web-frontend/react-vtstudio/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './assets/css/App.css'
+import { Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import Download from './pages/Download.jsx'
+import Sobre from './pages/Sobre.jsx'
+import Vtstudio from './pages/VTStudio.jsx'
 
 function App() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/download' element={<Download />} />
+                <Route path='/sobre' element={<Sobre />}/> 
+                <Route path='/vtstudio' element={<Vtstudio />}/> 
+            </Routes>
+            <Footer />
+        </>
     );
-
 }
 
 export default App
