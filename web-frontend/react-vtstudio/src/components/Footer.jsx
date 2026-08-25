05bb5fb7bf0d82f3fbe5c6d956ff8df5bff83a9c
Natan Nunes <nunesnatan905@gmail.com>
2026-08-25 10:53:10 -0300

    feat: Adicionado o componente footer

---
+++ web-frontend/react-vtstudio/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer>
+            <p>©Todos direitos reservados a Vandata©</p>
+            <a href="https://github.com">GITHUB</a>
+        </footer>
+    );
+}
+
+export default Footer
