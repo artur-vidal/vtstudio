e466a4366683ec8b09beb88482596032d22ef810
Natan Nunes <nunesnatan905@gmail.com>
2026-09-01 09:00:39 -0300

    feat: Aba download com informações extras

--- web-frontend/react-vtstudio/src/pages/Download.jsx
+++ web-frontend/react-vtstudio/src/pages/Download.jsx
@@ -1,6 +1,10 @@
+import { useLocation } from 'react-router-dom'
 import dados from '../assets/imgs/dados-rpg.jpg'
+import board from '../assets/imgs/board-dados.jpg'
 
 function Download() {
+    const location = useLocation();
+    const isPaginaDownload = location.pathname === '/download';
     return (
         <>
             <section className="download" id="download">
@@ -19,6 +23,26 @@ function Download() {
                     <a href="#" className="Download">Download</a>
                 </div>
             </section>
+
+            {isPaginaDownload && (
+                <section className='instalacao'>
+                    <div className='texto-cima'>
+                        <h2>Instalação</h2>
+                        <p>
+                            Após baixar o arquivo, faça seu login ou registre-se no aplicativo, dentro do app,<br />
+                            realize as configurações necessárias
+                            
+                        </p>
+                    </div>
+                    <img src={board} alt="Board de dados de rpg" />
+                    <div className='texto-baixo'>
+                        <p>
+                            Abra este link para o tutorial de instalação
+                        </p>
+                        <a href="#">Tutorial</a>
+                    </div>
+                </section>
+            )}
         </>
     )
 }
