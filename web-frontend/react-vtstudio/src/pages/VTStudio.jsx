dda35319d7b18859757c803827d35aa46564ba67
Natan Nunes <nunesnatan905@gmail.com>
2026-09-01 09:17:40 -0300

    feat: VTStudio conteúdo extra com estilização

--- web-frontend/react-vtstudio/src/pages/VTStudio.jsx
+++ web-frontend/react-vtstudio/src/pages/VTStudio.jsx
@@ -1,6 +1,11 @@
+import { useLocation } from 'react-router-dom'
 import livro from '../assets/imgs/rpg-de-mesa.webp'
+import jogo from '../assets/imgs/jogo-de-rpg.jpg'
 
 function Vtstudio() {
+    const location = useLocation();
+    const isPaginaVtstudio = location.pathname === '/vtstudio';
+
     return (
         <>
             <section className="Vtstudio" id="Vtstudio">
@@ -16,6 +21,17 @@ function Vtstudio() {
                     músicas ou até videos
                 </p>
             </section>
+            {isPaginaVtstudio && (
+                <section className='vtstudio-extra'>
+                    <h2>Propósito</h2>
+                    <img src={jogo} alt="Jogo de RPG com mapa e dados de fundo" />
+                    <p>
+                        O aplicativo vem como uma forma simples e intuitiva de se jogar RPG <br />
+                        hoje em dia, com funções que você sempre procurou e com fácil manuseio.
+                    </p>
+                </section>
+            )
+            }
         </>
     )
 }
