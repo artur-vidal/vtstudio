e3345b55e9b7904369670b995cc1ec286e4eb88c
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-22 11:01:17 -0300

    feat: adiciona rotas e controller - Controller 'FeedbackController' criado com as funções index() e store() - Grupo de rotas criado, com GET /api/feedbacks e POST /api/feedbacks

---
+++ api/app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    public function index() {
+        // implementar
+    }
+
+    public function store() {
+        // implementar
+    }
+}
