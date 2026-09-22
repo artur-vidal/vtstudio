d0d05c4c401544939b958cf4f3be42269df0c6c7
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-22 11:26:07 -0300

    feat: implementa FeedbackController@store - A rota recebe um parâmetro "texto" e cria um feedback usando esse texto e o possível usuário logado.

--- api/app/Http/Controllers/FeedbackController.php
+++ api/app/Http/Controllers/FeedbackController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feedback;
 use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
@@ -10,7 +11,19 @@ class FeedbackController extends Controller
         // implementar
     }
 
-    public function store() {
-        // implementar
+    public function store(Request $request) {
+        $data = $request->validate([
+            'texto' => ['required', 'string', 'max:65535']
+        ]);
+
+        $feedback = Feedback::create([
+            'texto' => $data['texto'],
+            'usuario_id' => $request->user()?->id
+        ]);
+
+        return response()->json([
+            'message' => 'Feedback postado! Obrigado por contribuir para o VTStudio!',
+            'data' => $feedback->toResource()
+        ], 201);
     }
 }
