8c4974813a7096ecbcdc40877c32cf1c229db35b
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-22 11:51:51 -0300

    fix: corrige classe vinculada ao middleware 'loose-api-token'

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ApiTokenMiddleware;
+use App\Http\Middleware\LooseAuthMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'api-token' => ApiTokenMiddleware::class,
-            'loose-api-token' => ApiTokenMiddleware::class,
+            'loose-api-token' => LooseAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
