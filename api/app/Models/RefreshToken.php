efb804a92eb8ff5790685efbcc434ed0a5f8ea89
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-22 09:58:19 -0300

    fix: corrige nomes de campos pelo código - Troca ocorrências de 'password' para 'senha' em validações em ApiAuthController - Troca 'name' por 'nome' em UserResource - Especifica nome de tabela nos models User e RefreshToken - Desabilita timestamps nos models User e RefreshToken

--- api/app/Models/RefreshToken.php
+++ api/app/Models/RefreshToken.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RefreshToken extends Model
 {
+    public $timestamps = false;
+    protected $table = 'refresh_tokens';
+    
     protected $guarded = ['id', 'created_at', 'updated_at'];
     protected $casts = ['expires_at' => 'datetime'];
 
     public function user(): BelongsTo {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'usuario_id');
     }
 }
