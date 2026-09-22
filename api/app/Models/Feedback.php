70c0a927d813e496847998622463b001e08d02a7
Artur Vidal <arturvidalcontato@gmail.com>
2026-09-22 10:51:07 -0300

    feat: cria e configura model Feedback

---
+++ api/app/Models/Feedback.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Feedback extends Model
+{
+    public $timestamps = false;
+
+    protected $guarded = ['id', 'created_at'];
+    protected $casts = [
+        'created_at' => 'datetime'
+    ];
+    
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
+}
